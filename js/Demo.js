b90ea986db714b06b08aa87b2d270e9c18bd835e
zhaosongxun <songxunzhao1991@gmail.com>
2020-02-04 22:23:00 +0800

    Added missing files

--- js/Demo.js
+++ js/Demo.js
@@ -50,25 +50,4 @@ $(document).ready(function () {
         $.removeCookie('box', { path: '/' });
         $.removeCookie('background', { path: '/' });
     });
-
-    var html = '' +
-        '<div class="demo">' +
-        '<a href="#" class="settings">' +
-        '<i class="fa fa-cog fa-spin"></i>' +
-        '</a>' +
-        '<h5>SELECT A COLOR</h5>' +
-        '<a href="#" class="color" data-color="yellow.css" style="background-color:#ffb100"></a>' +
-        '<a href="#" class="color" data-color="green2.css" style="background-color:#52bf00"></a>' +
-        '<a href="#" class="color" data-color="red.css" style="background-color:#d81300"></a>' +
-        '<a href="#" class="color" data-color="blue.css" style="background-color:#425cbb"></a>' +
-        '<a href="#" class="color" data-color="Purple.css" style="background-color:#BF55EC"></a>' +
-        '<a href="#" class="color" data-color="turquoise.css" style="background-color:#00b4d9"></a>' +
-        '<a href="#" class="color" data-color="green.css" style="background-color:#029A83"></a>' +
-        '<a href="#" class="color" data-color="pink.css" style="background-color:#f62459"></a>' +
-        '<a href="#" class="color" data-color="orange.css" style="background-color:#ff5a14"></a>' +
-        '<a href="#" class="color" data-color="grey.css" style="background-color:#ABB7B7"></a>' +
-        '<hr />' +
-        '<a href="#" class="reset btn btn-sm btn-info p-l-30 p-r-30">Reset</a>' +
-        '</div>';
-    $('body').append(html);
 });
